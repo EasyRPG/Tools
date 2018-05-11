87712b90b22c6c05aec18884452fec969bbe4ae8
Carsten Teibes <dev@f4ke.de>
2018-05-11 07:24:40 +0200

    Use C++ headers, add missing string header

--- lmu2png/src/sdlxyz.cpp
+++ lmu2png/src/sdlxyz.cpp
@@ -15,8 +15,9 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "sdlxyz.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <zlib.h>
 
 static SDL_Surface* XYZLoaderMakeSurface(int w, int h, unsigned char * data) {
